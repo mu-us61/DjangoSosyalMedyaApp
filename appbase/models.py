db413d0d9553528587878ed0f730eb5d798ffd50
muhammet <mavibirsaat@gmail.com>
2025-04-10 12:27:24 +0300

    Post modeli olusturuldu

--- appbase/models.py
+++ appbase/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+import uuid
+from django.utils import timezone
 
 # Create your models here.
 
@@ -7,7 +9,7 @@ User = get_user_model()
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(to=User, on_delete=models.CASCADE)
+    user = models.OneToOneField(to=User, on_delete=models.CASCADE, related_name="profile")
     bio = models.TextField(blank=True)
     picture = models.ImageField(upload_to="profilePictures/", blank=True, null=True, default="default_profile.png")
     location = models.CharField(max_length=100, blank=True)
@@ -17,3 +19,15 @@ class Profile(models.Model):
             return f"Profile of {self.user.username}"
         else:
             return "Profile without a user"
+
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name="posts")
+    image = models.ImageField(upload_to="post_images/")
+    caption = models.TextField(blank=True, null=True)
+    likes = models.ManyToManyField(to=User, related_name="liked_posts", blank=True)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.user.username}'s post"
